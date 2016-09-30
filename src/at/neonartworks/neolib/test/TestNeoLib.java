d0ec8e8877df1d0fcf9456df6d7337a5727aa7aa
Alexander Daum <daumalex10200@gmail.com>
2016-09-30 08:27:25 +0200

    Created Test Case for NeoPassword

--- src/at/neonartworks/neolib/test/TestNeoLib.java
+++ src/at/neonartworks/neolib/test/TestNeoLib.java
@@ -1,6 +1,7 @@
 package at.neonartworks.neolib.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import at.neonartworks.neolib.BigRandom;
 import at.neonartworks.neolib.NeoMath;
+import at.neonartworks.neolib.password.NeoPassword;
 
 public class TestNeoLib {
 
@@ -23,7 +25,7 @@ public class TestNeoLib {
 		BigInteger min = new BigInteger("-1000000000000000000000000000000");
 		BigInteger max = new BigInteger("1000000000000000000000000000000");
 		BigRandom r = new BigRandom().setDefaultFrom(min).setDefaultTo(max);
-		for (int i = 0; i < 1000000; i++) {
+		for (int i = 0; i < 10000; i++) {
 			BigInteger random = r.nextBInt();
 //			System.out.println(random);
 			if (random.compareTo(min) < 0 || random.compareTo(max) > 0) {
@@ -37,7 +39,7 @@ public class TestNeoLib {
 		BigDecimal min = new BigDecimal("-12345678901234567890.123456789");
 		BigDecimal max = new BigDecimal("12345678901234567890.123456789");
 		BigRandom r = new BigRandom().setDefaultFrom(min).setDefaultTo(max);
-		for (int i = 0; i < 1000000; i++) {
+		for (int i = 0; i < 10000; i++) {
 			BigDecimal random = r.nextBDecimal(2);
 //			System.out.println(random);
 			if (random.compareTo(min) < 0 || random.compareTo(max) > 0) {
@@ -51,6 +53,19 @@ public class TestNeoLib {
 		long l1 = 123;
 		String l1S =NeoMath.fillLongWith0(l1);
 		assertEquals("0000000000000000123", l1S);
+	}
+	@Test
+	public void testPw(){
+		NeoPassword pw = new NeoPassword();
+		String orig = pw.generatePassword(25);
+		pw.setPassword(orig);
+		System.out.println(orig);
+		String encrypt = pw.encrypt(orig);
+		System.out.println(encrypt);
+		String decrypt = pw.decrypt(encrypt);
+		System.out.println(decrypt);
+		assertEquals(orig, decrypt);
+	
 	}
 
 }
