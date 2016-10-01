8c866f4c2acd4cffa7dbe9ecd2080b3480ae9535
Alexander Daum <daumalex10200@gmail.com>
2016-10-01 15:37:44 +0200

    Updated Comment for NeoPoint

--- src/at/neonartworks/neolib/math/NeoPoint.java
+++ src/at/neonartworks/neolib/math/NeoPoint.java
@@ -1,16 +1,34 @@
 package at.neonartworks.neolib.math;
 
 public class NeoPoint {
+	
+	/**
+	 * 
+	 * @author Florian Wagner
+	 * Stores a x and a y value for point calculations
+	 * 
+	 */
 	private double x, y;
 	
+	/**
+	 * @param xkoord
+	 * @param ykoord
+	 */
     public NeoPoint (double xkoord, double ykoord) {
         this.x = xkoord;
         this.y = ykoord;
     }
+    /**
+     * @return the x - Value
+     */
     
     public double getX() {
         return x;
     }
+    /**
+     * @return the y - Value
+     */
+    
     public double getY() {
         return y;
     }
