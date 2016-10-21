3167e1aabd9e8eada6d07611deb1106db90a1add
Alexander Daum <daumalex10200@gmail.com>
2016-10-21 11:43:43 +0200

    small Changes

--- src/at/neonartworks/neolib/NeoMac.java
+++ src/at/neonartworks/neolib/NeoMac.java
@@ -18,7 +18,6 @@ public class NeoMac {
 			String[] result = line.split(",");
 			address =result[0].replace('"', ' ').trim();
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}        
 		return address;
