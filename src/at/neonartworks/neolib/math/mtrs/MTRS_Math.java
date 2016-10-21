053583814f23436250cd4adc4cb74e6d7d11cbe6
Alexander Daum <daumalex10200@gmail.com>
2016-10-21 11:38:13 +0200

    few Changes

--- src/at/neonartworks/neolib/math/mtrs/MTRS_Math.java
+++ src/at/neonartworks/neolib/math/mtrs/MTRS_Math.java
@@ -1,15 +1,23 @@
 package at.neonartworks.neolib.math.mtrs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MTRS_Math {
-	private final 
-	List<Double> values;
+	private final List<Double> values;
 
 	public MTRS_Math(List<Double> values) {
-		this.values = values;
+		this.values = new ArrayList<Double>(values);
 	}
 
+	public void add(double value) {
+		this.values.add(value);
+	}
+
+	/**
+	 * 
+	 * @return The average of the Values (germ. Mittelwert)
+	 */
 	public double Average() {
 		double sum = 0;
 		for (int i = 0; i < values.size(); i++) {
@@ -18,6 +26,10 @@ public class MTRS_Math {
 		return sum / values.size();
 	}
 
+	/**
+	 * 
+	 * @return The Abolute Average Value (germ. Gleichrichtwert)
+	 */
 	public double AAV() {
 		double sum = 0;
 		for (int i = 0; i < values.size(); i++) {
@@ -26,6 +38,10 @@ public class MTRS_Math {
 		return sum / values.size();
 	}
 
+	/**
+	 * 
+	 * @return The Root-Mean-Square (germ. Effektivwert)
+	 */
 	public double RMS() {
 		double sum = 0;
 		for (int i = 0; i < values.size(); i++) {
@@ -35,21 +51,55 @@ public class MTRS_Math {
 		return Math.sqrt(sum);
 	}
 
-	public double Ripple() {
+	/**
+	 * 
+	 * @return The Ripple (germ. Welligkeit)
+	 */
+	public double ripple() {
 		double rms = RMS();
 		double GLW = AAV();
 		return (Math.sqrt(rms * rms - GLW * GLW) / GLW);
 	}
+
 	/**
 	 * Calculates the power of an Alternating Electric Signal
-	 * @param The missing Component ether the voltage or the current
+	 * 
+	 * @param The
+	 *            missing Component ether the voltage or the current
 	 * @return
 	 */
-	public double Power(List<Double> UI){
+	public double power(List<Double> UI) {
 		double sum = 0;
 		for (int i = 0; i < values.size(); i++) {
-			sum += values.get(i)*UI.get(i);
+			sum += values.get(i) * UI.get(i);
 		}
 		return sum / values.size();
 	}
+
+	/**
+	 * 
+	 * @return The standard deviation (germ. Standardabweichung)
+	 */
+	public double standardDeviation() {
+		double average = Average();
+		double diffSum = 0;
+		for (double value : values) {
+			diffSum += (value - average) * (value - average);
+		}
+		diffSum /= (values.size() - 1);
+		return Math.sqrt(diffSum);
+	}
+	/**
+	 * 
+	 * @return The Confidence Interval (germ. Vertrauensbereich)
+	 * 
+	 * @param tValue The t value, to be found in a Table
+ 	 */
+	public double convInterval(double tValue){
+		double stdDev = standardDeviation();
+		double result;
+		result = tValue / Math.sqrt(values.size());
+		return result * stdDev;
+	}
+	
 }
